7ac249aae329c628ae0d6c8d23605807db0bef65
Vachek-hooves <vachek0602@gmail.com>
2025-01-21 18:45:46 +0200

    init projce

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
     <AppProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
-          {/* <Stack.Screen name="IntroScreen" component={IntroScreen} /> */}
+          <Stack.Screen name="IntroScreen" component={IntroScreen} />
           <Stack.Screen name="TabNavigation" component={TabNavigation} />
         </Stack.Navigator>
       </NavigationContainer>
--- AppScreens/Stack/IntroScreen.js
+++ AppScreens/Stack/IntroScreen.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const IntroScreen = () => {
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {useEffect} from 'react';
+const IntroScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('TabNavigation');
+    }, 3000);
+  }, []);
   return (
-    <View>
-      <Text>IntroScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+      source={require('../../assets/images/flashScreen/SplachScreen.png')}>
+      <Text style={styles.title}>Aristo Craft Up</Text>
+    </ImageBackground>
+  );
+};
 
-export default IntroScreen
+export default IntroScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 42,
+    fontWeight: 'bold',
+    color: 'white',
+    marginTop: 150,
+  },
+});
